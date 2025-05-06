d91f94c75686f5bef5f7cf78aad3522edf42fa3c
Dzima-G <dgukalin@gmail.com>
2025-05-06 19:54:45 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import sys
 import time
 
 import requests
@@ -9,93 +8,121 @@ from dotenv import load_dotenv
 
 
 class TelegramLogsHandler(logging.Handler):
-
-    def __init__(self, tg_bot, chat_id):
+    def __init__(self, error_telegram_token, telegram_chat_id):
         super().__init__()
-        self.chat_id = chat_id
-        self.tg_bot = tg_bot
+        self.tg_bot_token = error_telegram_token
+        self.tg_chat_id = telegram_chat_id
+        self.error_tg_bot = telegram.Bot(token=error_telegram_token)
 
     def emit(self, record):
-        log_entry = self.format(record)
-        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+        try:
+            log_entry = self.format(record)
+            self.error_message(log_entry)
+        except Exception as e:
+            print(f"TelegramLogsHandler error: {e}")
+
+    def error_message(self, message):
+        self.error_tg_bot.send_message(text=message, chat_id=self.tg_chat_id)
 
 
-def get_long_polling_response(url, devman_token, timestamp, tg_bot):
+def get_long_polling_response(
+    url, devman_token, timestamp, tg_bot, tg_chat_id
+):
     headers = {
-        'Authorization': f'Token {devman_token}',
+        "Authorization": f"Token {devman_token}",
     }
     while True:
         payload = {
-            'timestamp': timestamp,
+            "timestamp": timestamp,
         }
         response = requests.get(url, headers=headers, params=payload)
         response.raise_for_status()
         response = response.json()
-        if response.get('status') in 'timeout':
-            timestamp = response.get('timestamp_to_request')
-        elif response.get('status') in 'found':
-            timestamp = response.get('last_attempt_timestamp')
-            new_attempts = response.get('new_attempts')[0]
-            lesson_title = new_attempts.get('lesson_title')
-            verification_status = new_attempts.get('is_negative')
-            lesson_url = new_attempts.get('lesson_url')
-            send_message(lesson_title, verification_status, lesson_url, tg_bot)
+        if response.get("status") == "timeout":
+            timestamp = response.get("timestamp_to_request")
+        elif response.get("status") == "found":
+            timestamp = response.get("last_attempt_timestamp")
+            new_attempts = response.get("new_attempts")[0]
+            lesson_title = new_attempts.get("lesson_title")
+            verification_status = new_attempts.get("is_negative")
+            lesson_url = new_attempts.get("lesson_url")
+            send_message(
+                lesson_title,
+                verification_status,
+                lesson_url,
+                tg_bot,
+                tg_chat_id,
+            )
 
 
-def send_message(lesson_title, verification_status, lesson_url, tg_bot):
+def send_message(
+    lesson_title, verification_status, lesson_url, tg_bot, tg_chat_id
+):
     if verification_status:
         tg_bot.send_message(
-            text=f'У вас проверили работу «{lesson_title}»!\n\n'
-                 f'К сожалению, в работе нашлись ошибки.'
-                 f'\nСсылка на работу: {lesson_url}',
-            chat_id=telegram_chat_id)
+            text=f"У вас проверили работу «{lesson_title}»!\n\n"
+            f"К сожалению, в работе нашлись ошибки."
+            f"\nСсылка на работу: {lesson_url}",
+            chat_id=tg_chat_id,
+        )
     else:
         tg_bot.send_message(
-            text=f'У вас проверили работу «{lesson_title}»!\n\n'
-                 f'Преподавателю все понравилось, можно приступать '
-                 f'к следующему уроку.\n'
-                 f'Ссылка на работу: {lesson_url}',
-            chat_id=telegram_chat_id)
+            text=f"У вас проверили работу «{lesson_title}»!\n\n"
+            f"Преподавателю все понравилось, можно приступать "
+            f"к следующему уроку.\n"
+            f"Ссылка на работу: {lesson_url}",
+            chat_id=tg_chat_id,
+        )
 
 
 if __name__ == "__main__":
     load_dotenv()
 
-    devman_api_token = os.environ['DEVMAN_API_TOKEN']
-    telegram_token = os.environ['TELEGRAM_TOKEN']
-    telegram_chat_id = os.environ['TG_CHAT_ID']
+    devman_api_token = os.environ["DEVMAN_API_TOKEN"]
+    telegram_token = os.environ["TELEGRAM_TOKEN"]
+    error_telegram_token = os.environ["ERROR_TELEGRAM_TOKEN"]
+    telegram_chat_id = os.environ["TG_CHAT_ID"]
 
     telegram_bot = telegram.Bot(token=telegram_token)
+    error_telegram_bot = telegram.Bot(token=error_telegram_token)
 
-    logger = logging.getLogger('Logger')
-    logging.basicConfig(level=logging.INFO)
-    logger.addHandler(TelegramLogsHandler(telegram_bot, telegram_chat_id))
+    url = "https://dvmn.org/api/long_polling/"
 
-    url = 'https://dvmn.org/api/long_polling/'
+    logger = logging.getLogger("logger")
+    logger.setLevel(logging.INFO)
+    tg_handler = TelegramLogsHandler(error_telegram_token, telegram_chat_id)
+    tg_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(message)s")
+    tg_handler.setFormatter(formatter)
+    logger.addHandler(tg_handler)
+
+    logger.info("Бот успешно запущен!")
     now_timestamp = time.time()
 
     while True:
         try:
-            logger.info('Бот запущен')
             get_long_polling_response(
                 url,
                 devman_api_token,
                 now_timestamp,
-                telegram_bot)
+                telegram_bot,
+                telegram_chat_id,
+            )
         except requests.exceptions.ReadTimeout:
-            logger.warning('Ожидание ответа от сервера истекло,'
-                           'повторный запрос отправлен!')
+            logger.warning(
+                "Ожидание ответа от сервера истекло,"
+                "повторный запрос отправлен!"
+            )
             continue
-        except requests.exceptions.HTTPError as error:
-            logger.error('Бот упал с ошибкой:')
-            print(error, file=sys.stderr)
+        except requests.exceptions.HTTPError as err:
+            logger.error(f"Бот упал с ошибкой:{err}")
         except requests.exceptions.ConnectionError:
-
-            logger.warning('Не удается подключиться к серверу!'
-                           ' Повторное подключение через 10 секунд.')
+            logger.warning(
+                "Не удается подключиться к серверу!"
+                " Повторное подключение через 10 секунд."
+            )
             time.sleep(10)
             continue
         except Exception as err:
-            logger.error('Бот упал с ошибкой:')
-            logger.exception(err)
+            logger.error(f"Бот упал с ошибкой:{err}")
             break
